d293b3501a818e4a76cd740e07851104ed4bcd10
knightofknee <bricarlis@gmail.com>
2018-02-21 11:41:35 -0600

    mostly functional

--- src/components/Poll.vue
+++ src/components/Poll.vue
@@ -1,14 +1,15 @@
 <template>
 <div>
   <h1>hiya partner</h1>
-  <h2>Question: {{this.question}}</h2>
+  <h2>Question: {{question}}</h2>
   <h3>
-    <div v-for="answer in this.answers">
-      <h5>{{answer.answer}}</h5>
+    <div v-for="(answer, index) in answers">
+      <button :value="index" v-on:click="show($event)" >{{answer.answer}}</button>
       <h5 v-if="showCount">{{answer.count}}</h5>
     </div>
   </h3>
-  <button v-on:click="show">Show results</button>
+  <!-- <button v-if="!showCount" v-on:click="show($event)">Submit and reveal results</button> -->
+  <!-- change this to be a submit button, updating the count with a method -->
 
 </div>
 </template>
@@ -30,20 +31,35 @@ export default {
     created() {
       db.collection('Polls')
       .where(firebase.firestore.FieldPath.documentId(), '==', this.$route.params.pollId)
-      .get()
-      .then((querySnapshot) => {
+      .onSnapshot((querySnapshot) => {
+        if (querySnapshot.empty) {alert('It appears you are looking for a poll that does not exist')}
+        else {
         querySnapshot.forEach((doc) =>{
           this.question = doc.data().question
           this.answers = doc.data().answers
-          })
-      });
+          })}
+      })
+      // .catch(err => {alert('It appears you are looking for a poll that does not exist: ' + err)})
     },
     methods: {
-      show: function() {
+      show: function(event) {
+        if (event) event.preventDefault()
+        this.answers[event.target.value].count++
         this.showCount = !this.showCount
+        let pollReference = db.collection('Polls').doc(this.$route.params.pollId).update({
+          answers: this.answers
+        })
+        .then(() => {console.log('success')})
+        .catch(console.log)
+        // .where(firebase.firestore.FieldPath.documentId(), "==", this.$route.params.pollId)
+        // .set()
+        // console.log('test', pollReference)
       }
     }
 }
+
+      // .get()
+      // .then((querySnapshot) => {
 </script>
 
 <style scoped>
