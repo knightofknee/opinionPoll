4c80613ad976ecd523d253cd3217a2954cde2dff
knightofknee <bricarlis@gmail.com>
2018-02-21 02:58:12 -0600

    admin functionality complete

--- src/components/ManagePolls.vue
+++ src/components/ManagePolls.vue
@@ -1,5 +1,39 @@
 <template>
   <div>
+    <button v-on:click="show">Show Polls</button>
+    <table class="table full-width">
+      <thead>
+        <tr>
+          <th>Poll Number</th>
+          <th>Question</th>
+          <th>Answers and Counts</th>
+        </tr>
+      </thead>
+        <tbody v-if="this.showPolls" >
+          <tr v-for="poll in this.polls">
+            <td>{{poll.id}}</td>
+            <td>{{poll.data.question}}</td>
+            <td>
+              <table class="table">
+                <tr v-for="answer in poll.data.answers">
+                  <td class="nested-table">{{answer.answer}}</td>
+                  <td class="nested-table">{{answer.count}}</td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+    </table>
+    <!-- <div class="columns" v-for="(poll, index) in this.polls">
+        <div v-bind:key="poll.id" class="column">{{poll.id}}</div>
+        <div v-bind:key="index" class="column">{{poll.data.question}}</div>
+        <div v-bind:key="index" class="column">
+          <div v-for="(answer, index2) in poll.answers">
+            <div>ddd{{answer.answer}}</div>
+            <div>-{{answer.count}}</div>
+          </div>
+      </div>
+    </div> -->
     <div class="enter-poll">
     <h3>To create a new poll, enter a question and carot(^) delimited potential answers</h3>
     <h3 v-if="this.pollNumber">Your new poll number is {{this.pollNumber}}, note it for future use</h3>
@@ -22,10 +56,14 @@ export default {
       polls: [],
       question: '',
       answers: '',
-      pollNumber: ''
+      pollNumber: '',
+      showPolls: false
     }
   },
   methods: {
+    show: function() {
+      this.showPolls = !this.showPolls
+    },
     logout: function() {
       firebase.auth().signOut()
       .then(() => {
@@ -63,12 +101,26 @@ export default {
         count++
         })
     });
-  }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.table th {
+  text-align: center;
+}
+/* .table td {
+  border-right: 2px solid black;
+} */
+
+td {
+  text-align: center;
+}
+
+.nested-table {
+  width: 50%;
+}
 
 button {
   margin: 5px;
